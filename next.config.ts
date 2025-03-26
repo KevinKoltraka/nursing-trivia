bec41978e33f7039debdbc10aa4f56dd49ea6cb4
Kevin <kevikoltraka157@gmail.com>
2025-03-26 22:23:37 +0100

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  allowedDevOrigins: [
+    "http://localhost:3000",       // Local development URL
+    "http://192.168.100.39",       // Your local network IP (if accessing locally)
+    "https://your-deployed-url.com", // Add your deployed URL here
+  ],
 };
 
 export default nextConfig;
